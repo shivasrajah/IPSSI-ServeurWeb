e302499390c842bfdf6750aaae409b7a4ee80c43
Shiva <shiva.srajah@gmail.com>
2018-07-11 00:01:51 +0200

    Envoi du projet (Pas fini)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test IPSSI-ServeurWeb
+
+### Utilisation de redis pour load balancing
---
+++ part1/web/index.php
@@ -0,0 +1,13 @@
+<?php
+    $title = 'Site d\'exemple 1';
+?>
+<!Doctype>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+    </head>
+    <body>
+        <h1><?= $title ?></h1>
+    </body>
+</html>
